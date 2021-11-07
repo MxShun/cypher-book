b4208380d808d754ae2d9e2a87887487ea303c23
MxShun <16466322+MxShun@users.noreply.github.com>
2021-11-08 08:22:02 +0900

    postgresはコンパイル時は利用しない

--- infra/build.gradle.kts
+++ infra/build.gradle.kts
@@ -2,5 +2,5 @@ dependencies {
     implementation(project(":application"))
     implementation(project(":domain"))
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.postgresql:postgresql")
+    runtimeOnly("org.postgresql:postgresql")
 }
