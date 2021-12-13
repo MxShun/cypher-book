3c9daa4209ce56a8530f166f03ffdda825fedc26
MxShun <16466322+MxShun@users.noreply.github.com>
2021-12-13 21:43:47 +0900

    dataが見つからなかった場合の例外ハンドリング

--- web/src/main/kotlin/cypher/book/web/advice/ExceptionHandlerAdvice.kt
+++ web/src/main/kotlin/cypher/book/web/advice/ExceptionHandlerAdvice.kt
@@ -1,5 +1,6 @@
 package cypher.book.web.advice
 
+import cypher.book.domain.error.DataNotFountException
 import cypher.book.web.response.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -13,4 +14,9 @@ class ExceptionHandlerAdvice {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     fun handleConstraintViolationException(exception: ConstraintViolationException): ErrorResponse =
         ErrorResponse(reason = "invalid parameter: detail: [${exception.message}]")
+
+    @ExceptionHandler(DataNotFountException::class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    fun handleDataNotFountException(exception: DataNotFountException): ErrorResponse =
+        ErrorResponse(reason = exception.message!!) // DataNotFountException しか補足しないため non-null assertion を許容する
 }
