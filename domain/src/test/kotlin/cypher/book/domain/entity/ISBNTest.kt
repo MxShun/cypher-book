05334ebf9b386dce7e099618b2a5430ae2ad4115
MxShun <16466322+MxShun@users.noreply.github.com>
2021-12-18 16:56:25 +0900

    ISBNのUT実装

---
+++ domain/src/test/kotlin/cypher/book/domain/entity/ISBNTest.kt
@@ -0,0 +1,88 @@
+package cypher.book.domain.entity
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
+internal class ISBNTest {
+    @Nested
+    @DisplayName("validateで桁数を検証できること")
+    inner class Validate {
+        @Test
+        @DisplayName("validateでISBNの有効な文字列の桁数が10桁の場合はISBN-10として例外をスローしないこと")
+        fun validateISBN10Length() {
+            assertDoesNotThrow("想定しない例外スロー") {
+                ISBN(value = "ISBN4-7897-1537-5")
+            }
+        }
+
+        @Test
+        @DisplayName("validateでISBNの有効な文字列の桁数が13桁の場合はISBN-13として例外をスローしないこと")
+        fun validateISBN13Length() {
+            assertDoesNotThrow("想定しない例外スロー") {
+                ISBN(value = "ISBN978-3-16-148410-0")
+            }
+        }
+
+        @Test
+        @DisplayName("validateでISBNの有効な文字列の桁数が不正な場合は例外をスローすること")
+        fun validateInvalidISBNLength() {
+            assertThrows<IllegalArgumentException>("想定した例外のスローに失敗") {
+                ISBN(value = "999")
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("checkChecksumISBN10でISBN-10のチェックディジット計算検証ができること")
+    inner class CheckChecksumISBN10 {
+        @Test
+        @DisplayName("checkChecksumISBN10でISBN-10の値が有効な場合は例外をスローしないこと")
+        fun checkChecksumValidISBN10() {
+            assertDoesNotThrow("想定しない例外スロー") {
+                ISBN("ISBN4-7897-1537-5")
+            }
+        }
+
+        @Test
+        @DisplayName("checkChecksumISBN10でISBN-10の値が不正な場合は例外をスローしないこと")
+        fun checkChecksumInvalidISBN10() {
+            assertThrows<IllegalArgumentException>("想定しない例外スロー") {
+                ISBN("9-99-9999999")
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("checkChecksumISBN13でISBN-13のチェックディジット計算検証ができること")
+    inner class CheckChecksumISBN13 {
+        @Test
+        @DisplayName("checkChecksumISBN13でISBN-13の値が有効な場合は例外をスローしないこと")
+        fun checkChecksumValidISBN13() {
+            assertDoesNotThrow("想定しない例外スロー") {
+                ISBN("ISBN978-3-16-148410-0")
+            }
+        }
+
+        @Test
+        @DisplayName("checkChecksumISBN13でISBN-13の値が不正な場合は例外をスローしないこと")
+        fun checkChecksumInvalidISBN13() {
+            assertThrows<IllegalArgumentException>("想定しない例外スロー") {
+                ISBN("999-9-99-999999-9")
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("digitsでISBNとして有効な文字列が取得できること")
+    fun digits() {
+        assertEquals(
+            "9783161484100",
+            ISBN(value = "ISBN978-3-16-148410-0").digits(),
+            "想定した値の取得に失敗"
+        )
+    }
+}
